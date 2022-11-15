5e47d1681c2f0c2ad8586ab86736ab5b8e5d338c
Inna K <kuklina.inn@yandex.ru>
2022-11-15 20:10:25 +0300

    fix testing

--- src/test/java/MySpendingTest.java
+++ src/test/java/MySpendingTest.java
@@ -6,6 +6,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -13,24 +14,24 @@ public class MySpendingTest {
     private MySpending mySpending;
     private Map<String, String> categories;
     private DataRequest dataRequest;
-    private static String date;
 
-    @Test
+    @BeforeEach
     void load() {
         categories = TSVReader.readTSV(new File("categories.tsv"));
         mySpending = new MySpending(MySpending.categoryHashMap(categories));
-        date = new SimpleDateFormat("yyyy.MM.dd").format(new Date());
-        dataRequest = new DataRequest("морковь", date, 25);
+        String date = new SimpleDateFormat("yyyy.MM.dd").format(new Date());
+        dataRequest = new DataRequest("морковь", date, 0);
     }
 
     @Test
     void sumSpending() {
+        categories = TSVReader.readTSV(new File("categories.tsv"));
         mySpending.sumNewSpending(categories, dataRequest);
         Map<String, Long> spending = new HashMap<>();
         for (Map.Entry<String, String> category : categories.entrySet()) {
             spending.put(category.getValue(), 0L);
         }
-        spending.put("другое", 50L);
+        spending.put("другое", 0L);
         assertEquals(spending, mySpending.getSpending());
     }
 }
